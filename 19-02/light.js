b9267b17f8b5b34380083d5ceba139e5f924e09c
Daniel <dfpires@gmail.com>
2025-02-19 08:48:02 -0300

    imagens bulb

--- 19-02/light.js
+++ 19-02/light.js
@@ -1,12 +1,28 @@
-import {SafeAreaView, Image, Switch} from "react-native"
+import {SafeAreaView, Image, Switch, StyleSheet} from "react-native"
 import {useState} from "react"
+import bulbOn from "bulb-on.jpg"
+import bulbOff from "bulb-off.jpg"
 export default function Light(){
     // cria variável de estado com hook useState
     const [isEnabled, setIsEnabled] = useState(false)
+    const mudaValor = () => setIsEnabled(
+        (estadoAnterior) => !estadoAnterior)
+    
     return (
         <SafeAreaView>
-            <Image />
-            <Switch />
+            <Image 
+                fadeDuration={0}
+                source={isEnabled ? bulbOn : bulbOff} 
+                style={styles.image}/>
+            <Switch
+                onValueChange={mudaValor} 
+                value={isEnabled}/>
         </SafeAreaView>
     )
 }
+const styles = StyleSheet.create({
+    image: {
+        width: 300,
+        height: 300
+    }
+})
