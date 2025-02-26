f43c45e1dbb34bf0c8255a534372f2e0bc161757
Daniel <dfpires@gmail.com>
2025-02-26 09:02:29 -0300

    aula 26-02

--- 26-02/facebook.js
+++ 26-02/facebook.js
@@ -1,12 +1,35 @@
-import {SafeAreaView, Image} from "react-native"
-import ImagemFacebook from  
+import {SafeAreaView, Image, StyleSheet, Dimensions} from "react-native"
+import ImagemFacebook from "./facebook-banner.jpg" 
+
+const larguraTela = Dimensions.get("window").width
+const alturaBanner = (larguraTela / 75) * 46 // razão da imagem - 75 x 46
+
 export default function Facebook(){
     return (
         <>
             <Image source={ImagemFacebook} style={styles.banner}/>
-            <SafeAreaView>
-            
+            <SafeAreaView style={styles.container}>
+                <View style={styles.content}>
+                    <TextInput/>
+                    <TextInput/>
+                    <TouchableOpacity> </TouchableOpacity>
+                    <TouchableOpacity> </TouchableOpacity>
+                    <TouchableOpacity> </TouchableOpacity>
+                </View>
+                <View style={styles.footer}>
+                    <View style={styles.divider}>
+
+                    </View>
+                    <TouchableOpacity> </TouchableOpacity>
+                </View>
             </SafeAreaView>       
         </>
     )
 }
+const styles = StyleSheet.create({
+    banner: {
+        resizeMode: "contain",
+        width: "100%",
+        height: alturaBanner
+    }
+})
