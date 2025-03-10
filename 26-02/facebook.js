de51022754d546a986e84150113b22a215f22d93
Daniel <dfpires@gmail.com>
2025-03-10 12:11:48 -0300

    Login Facebook

--- 26-02/facebook.js
+++ 26-02/facebook.js
@@ -1,4 +1,4 @@
-import {SafeAreaView, Image, StyleSheet, Dimensions} from "react-native"
+import {SafeAreaView, Image, StyleSheet, Dimensions, View, TextInput, TouchableOpacity, Text} from "react-native"
 import ImagemFacebook from "./facebook-banner.jpg" 
 
 const larguraTela = Dimensions.get("window").width
@@ -10,11 +10,19 @@ export default function Facebook(){
             <Image source={ImagemFacebook} style={styles.banner}/>
             <SafeAreaView style={styles.container}>
                 <View style={styles.content}>
-                    <TextInput/>
-                    <TextInput/>
-                    <TouchableOpacity> </TouchableOpacity>
-                    <TouchableOpacity> </TouchableOpacity>
-                    <TouchableOpacity> </TouchableOpacity>
+                    <TextInput style={styles.input} 
+                               placeholder="Número de telefone ou e-mail"/>
+                    <TextInput style={styles.input} 
+                               placeholder="Senha" secureTextEntry={true}/>
+                    <TouchableOpacity style={styles.button}> 
+                        <Text style={styles.text}> Conecte-se </Text> 
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}> 
+                        <Text style={styles.text}> Esqueceu a senha </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}> 
+                        <Text style={styles.text}> Volte </Text> 
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.footer}>
                     <View style={styles.divider}>
@@ -31,5 +39,25 @@ const styles = StyleSheet.create({
         resizeMode: "contain",
         width: "100%",
         height: alturaBanner
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#cdcdcf",
+        color: "#333333",
+        fontSize: 16,
+        height: 44,
+        paddingHorizontal: 15
+    },
+    button: {
+        height: 42,
+        borderRadius: 6,
+        backgroundColor: "#1977f3",
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 15,
+        marginHorizontal: 25
+    },
+    text: {
+        color: "white"
     }
 })
