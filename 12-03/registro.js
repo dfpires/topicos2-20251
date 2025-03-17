368ec6b5ac2814a1d3d4c6dd784db8c55e1aed4e
Daniel <dfpires@gmail.com>
2025-03-17 11:47:43 -0300

    aula 12-03

--- 12-03/registro.js
+++ 12-03/registro.js
@@ -1,16 +1,37 @@
-import {SafeAreaView, View} from "react-native"
+import {SafeAreaView, View, Text, TextInput, TouchableOpacity} from "react-native"
 import {Formik} from "formik"
 import {styles} from "./styles"
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view"
 export default function Registro(){
     return (
         <>
           <SafeAreaView style={styles.topSafeArea}/>
-          <SafeAreaView>
-            <View>
-
+          <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}> Register </Text>
             </View>
             <Formik>
-
+              <KeyboardAwareScrollView>
+                <View>
+                  <Text> First Name </Text>
+                  <TextInput />
+                </View>
+                <View>
+                  <Text> Last Name </Text>
+                  <TextInput />
+                </View>  
+                <View>
+                  <Text> Password </Text>
+                  <TextInput />
+                </View>  
+                <View>
+                  <Text> Confirm Password </Text>
+                  <TextInput />
+                </View>
+                <TouchableOpacity>
+                  <Text> SUBMIT </Text>
+                </TouchableOpacity>
+              </KeyboardAwareScrollView>
             </Formik>
           </SafeAreaView>
         </>
