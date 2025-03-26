e9d779c7c24f1b979a9678b6aae6ef6dbf97748e
Daniel <dfpires@gmail.com>
2025-03-26 08:54:09 -0300

    Instagram

--- 26-03/App.js
+++ 26-03/App.js
@@ -1,16 +1,17 @@
-import {SafeAreaView, View, FlatList, TouchableOpacity, Image} from "react-native"
+import {SafeAreaView, View, FlatList, TouchableOpacity, Image, StyleSheet} from "react-native"
 import {Feather} from "@expo/vector-icons"
+import Constants from "expo-constants"
 export default function Instagram(){
     const INSTAGRAM_LOGO =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png";
 
     return ( 
-        <SafeAreaView>
-            <View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
                 <TouchableOpacity>
                     <Feather name="camera" size={24}/>
                 </TouchableOpacity>
-                <Image/>
+                <Image style={styles.logo} source={{uri: INSTAGRAM_LOGO}}/>
                 <TouchableOpacity>
                     <Feather name="send" size={24}/>
                 </TouchableOpacity>
@@ -19,3 +20,23 @@ export default function Instagram(){
         </SafeAreaView>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Constants.statusBarHeight
+    },
+    header: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#dbdbdb",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 16,
+        height: 44
+    },
+    logo: {
+        flex: 1,
+        height: 30,
+        resizeMode: "contain"
+    }
+})
