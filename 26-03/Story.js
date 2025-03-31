306cdd666326c41e109b325cdc13f33637f295e1
Daniel <dfpires@gmail.com>
2025-03-31 12:39:47 -0300

    Instagram

--- 26-03/Story.js
+++ 26-03/Story.js
@@ -1,14 +1,17 @@
-import {TouchableOpacity, View, Image, Text} from "react-native"
+import {TouchableOpacity, View, Image, Text, StyleSheet} from "react-native"
 import {Feather} from "@expo/vector-icons"
 export default function Story({avatar, name, isCreateStory = false, isSeen}){
     return (
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.user}>
             <View>
-                <View>
-                    <Image source={avatar}/>
+                <View style={[styles.avatarBorder, {
+                    borderColor: isCreateStory ? "transparent" : 
+                    isSeen ? "rgba(0,0,0,0.97)" : "#c73191"
+                }]}>
+                    <Image source={avatar} style={styles.avatar}/>
                     {
                         isCreateStory && (
-                            <View>
+                            <View style={styles.plusIcon}>
                                 <Feather name="plus" size={14} color="#fff"/>
                             </View>
                         )
@@ -19,5 +22,39 @@ export default function Story({avatar, name, isCreateStory = false, isSeen}){
                 </Text>
             </View>
         </TouchableOpacity>
+
     )
 }
+const styles = StyleSheet.create({
+    user: {
+        width: 80,
+        paddingHorizonta: 4
+    },
+    avatarBorder: {
+        width: 56,
+        height: 56,
+        borderRadius: 28,
+        borderWidth: 1,
+        margin: 4,
+        marginBottom: 8,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+    plusIcon: {
+        position: "absolute",
+        right: 0,
+        bottom: 0,
+        width: 18,
+        height: 18,
+        borderRadius: 10,
+        overflow: "hidden",
+         justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#3e95fe"
+    }          
+})
