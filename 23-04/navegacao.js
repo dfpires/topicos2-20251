6c27a34535d751a919e87c3b735929d3771e1ca6
Daniel <dfpires@gmail.com>
2025-04-23 08:56:54 -0300

    navegação

--- 23-04/navegacao.js
+++ 23-04/navegacao.js
@@ -11,28 +11,56 @@ function HomeScreen({navigation}) {
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text> Tela inicial </Text>
             <Button title="Café" onPress={() => 
-                navigation.navigate('Cafe')
+                navigation.navigate('Cafe', {tipo: 'Bourbon', sabor: 'caramelo'})
+            }/>
+            <Button title="Chocolate" onPress={() => 
+                navigation.navigate('Chocolate')
+            }/>
+            <Button title="Chá" onPress={() => 
+                navigation.navigate('Cha')
             }/>
         </View>
     )
 }
 // componente CafeScreen
-function CafeScreen({navigation}){
+function CafeScreen({route, navigation}){
+    const {tipo, sabor} = route.params
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text> Tela dos detalhes de Café </Text>
+            <Text> Tipo: {tipo} </Text>
+            <Text> Sabor: {sabor} </Text>
             <Button title="Voltar" onPress={() => navigation.goBack()}/>
         </View>
     )
 }
+// componente ChocolateScreen
+function ChocolateScreen({navigation}){
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text> Tela dos detalhes do Chocolate </Text>
+            <Button title="Voltar" onPress={() => navigation.goBack()}/>
+        </View>
+    )
+}
+// componente ChaScreen
+function ChaScreen({navigation}){
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text> Tela dos detalhes do Chá </Text>
+            <Button title="Voltar" onPress={() => navigation.goBack()}/>
+        </View>
+    )
+}
+
 export default function App(){
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Inicio">
                 <Stack.Screen name="Inicio" component={HomeScreen}/>
                 <Stack.Screen name="Cafe" component={CafeScreen}/>
-          {/*      <Stack.Screen name="Chocolate" component={ChocolateScreen}/>
-                <Stack.Screen name="Cha" component={ChaScreen}/> */}
+                <Stack.Screen name="Chocolate" component={ChocolateScreen}/>
+                <Stack.Screen name="Cha" component={ChaScreen}/> 
             </Stack.Navigator>
         </NavigationContainer>
     )
